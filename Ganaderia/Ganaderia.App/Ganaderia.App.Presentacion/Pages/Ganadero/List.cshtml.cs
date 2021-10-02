cca5c5b428454aac479e1cb34c8380241412b29a
davidtabares <jedata@hotmail.com>
2021-10-02 16:43:47 -0500

    v1.1.2 codificacion login

--- Ganaderia/Ganaderia.App/Ganaderia.App.Presentacion/Pages/Ganadero/List.cshtml.cs
+++ Ganaderia/Ganaderia.App/Ganaderia.App.Presentacion/Pages/Ganadero/List.cshtml.cs
@@ -4,13 +4,44 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Ganaderia.App.Dominio;
 
 namespace Ganaderia.App.Presentacion.Pages
 {
     public class ListModel : PageModel
     {
+        public List<Ganadero> Ganaderos { get; set;}
+
         public void OnGet()
         {
+            var ganadero = new Ganadero
+            {
+                Cedula = "14855647",
+                Nombres = "Sergio",
+                Apellidos = "Moreno",
+                NumeroTelefono = "3124568744",
+                Email = "sergiomintic@mintic.edu.co",
+                Contrasena = "123456",
+                Rol = Rol.ganadero,
+                RegistroFedegan = "AD123685447"
+            };
+
+            var ganadero1 = new Ganadero
+            {
+                Cedula = "45988321",
+                Nombres = "Susana",
+                Apellidos = "Palacio",
+                NumeroTelefono = "3104443355",
+                Email = "susana@mintic.edu.co",
+                Contrasena = "128856",
+                Rol = Rol.ganadero,
+                RegistroFedegan = "AD124515447"
+            };
+
+            Ganaderos = new List<Ganadero>();
+
+            Ganaderos.Add(ganadero);
+            Ganaderos.Add(ganadero1);
         }
     }
 }
